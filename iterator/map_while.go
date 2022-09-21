e7033f731fb8b4f671f2e0290a811879e05b62fa
Siddharth Kurella <siddharthkurella@gmail.com>
2022-09-20 22:17:02 -0400

    Implement map_while

---
+++ iterator/map_while.go
@@ -0,0 +1,28 @@
+package iterator
+
+import "github.com/sidkurella/goption/option"
+
+type mapWhileIterator[T any, U any] struct {
+	inner Iterator[T]
+	pred  func(T) option.Option[U]
+}
+
+// Returns an iterator yielding elements from inner, mapped by pred.
+// The iterator stops once pred returns Nothing. Yields elements while pred continues to return Some.
+func MapWhile[T any, U any](inner Iterator[T], pred func(T) option.Option[U]) *mapWhileIterator[T, U] {
+	return &mapWhileIterator[T, U]{
+		inner: inner,
+		pred:  pred,
+	}
+}
+
+func (m *mapWhileIterator[T, U]) Next() option.Option[U] {
+	return option.Match(m.inner.Next(),
+		func(s option.Some[T]) option.Option[U] {
+			return m.pred(s.Value)
+		},
+		func(n option.Nothing[T]) option.Option[U] {
+			return option.Nothing[U]{}
+		},
+	)
+}
