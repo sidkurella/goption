726517d3115ae04da784f52fa0502097c9335317
Siddharth Kurella <siddharthkurella@gmail.com>
2022-09-21 12:43:58 -0400

    Implement Scan

---
+++ iterator/scan.go
@@ -0,0 +1,31 @@
+package iterator
+
+import "github.com/sidkurella/goption/option"
+
+type scanIterator[T any, S any, U any] struct {
+	inner Iterator[T]
+	state *S
+	f     func(*S, T) option.Option[U]
+}
+
+// An iterator adapter similar to fold that holds internal state and produces a new iterator.
+// Calls f with the current state and the current item, and yields a new item mapped by the closure.
+// NOTE: The closure is responsible for updating the state; it is not automatically assigned the result of the closure.
+func Scan[T any, S any, U any](inner Iterator[T], initial S, f func(*S, T) option.Option[U]) *scanIterator[T, S, U] {
+	return &scanIterator[T, S, U]{
+		inner: inner,
+		state: &initial,
+		f:     f,
+	}
+}
+
+func (s *scanIterator[T, S, U]) Next() option.Option[U] {
+	return option.Match(s.inner.Next(),
+		func(v option.Some[T]) option.Option[U] {
+			return s.f(s.state, v.Value)
+		},
+		func(_ option.Nothing[T]) option.Option[U] {
+			return option.Nothing[U]{}
+		},
+	)
+}
