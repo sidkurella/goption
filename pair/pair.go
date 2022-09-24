de491602bc4b29d8e62f380b0361087ac5bbf859
Siddharth Kurella <siddharthkurella@gmail.com>
2022-09-24 15:46:45 -0400

    Implement pair From/Into

--- pair/pair.go
+++ pair/pair.go
@@ -5,3 +5,16 @@ type Pair[T any, U any] struct {
 	First  T
 	Second U
 }
+
+// Takes 2 values and returns a pair from them.
+func From[T any, U any](first T, second U) Pair[T, U] {
+	return Pair[T, U]{
+		First:  first,
+		Second: second,
+	}
+}
+
+// Returns a tuple of values from the given pair.
+func (p Pair[T, U]) Into() (T, U) {
+	return p.First, p.Second
+}
